d44ad899312eb34e9449fb6baa3550e89c304846
DAC Tran Ngoc (VTI.VMS) <dac.tranngoc@vti.vn>
2023-01-06 16:41:37 +0700

    ot lines view (group by)

--- models/ot_registration.py
+++ models/ot_registration.py
@@ -1,5 +1,8 @@
+import datetime
+import holidays
+
+import pytz
 from odoo import models, fields, api
-import datetime, holidays
 
 SECONDS_PER_HOUR = 3600
 
@@ -9,23 +12,47 @@ STATES_DICT = {'draft': 'Draft',
                'done': 'DL Approved',
                'refused': 'Refused'}
 
-CATEGORY_DICT = {'normal_day': 'Ngày bình thường',                      #18h30-22h
-                 'normal_day_morning': 'OT ban ngày (6h-8h30)',         #6h-8h30
-                 'normal_day_night': 'Ngày bình thường - Ban đêm',      #6h-8h30
-                 'saturday': 'Thứ 7',                                   #6h-22h
-                 'sunday': 'Chủ nhật',                                  #6h-22h
-                 'weekend_day_night': 'Ngày cuối tuần - Ban đêm',       #
-                 'holiday': 'Ngày lễ',                                  #
-                 'holiday_day_night': 'Ngày lễ - Ban đêm',              #
-                 'compensatory_normal': 'Bù ngày lễ vào ngày thường',   #
-                 'compensatory_night': 'Bù ngày lễ vào ban đêm',        #
+CATEGORY_DICT = {'normal_day': 'Ngày bình thường',
+                 'normal_day_morning': 'OT ban ngày (6h-8h30)',
+                 'normal_day_night': 'Ngày bình thường - Ban đêm',
+                 'saturday': 'Thứ 7',
+                 'sunday': 'Chủ nhật',
+                 'weekend_day_night': 'Ngày cuối tuần - Ban đêm',
+                 'holiday': 'Ngày lễ',
+                 'holiday_day_night': 'Ngày lễ - Ban đêm',
+                 'compensatory_normal': 'Bù ngày lễ vào ngày thường',
+                 'compensatory_night': 'Bù ngày lễ vào ban đêm',
                  'unknown': 'Không thể xác định'}
+
+# 6h-8h, 18h30-22h, 22h-6h, 6h-22h
+# morning, normal, night, day, saturday, sunday, weekend, holiday
+CATEGORY_CODE = {'normal_day': '01010000',
+                 'normal_day_morning': '01010000',
+                 'normal_day_night': '00100000',
+                 'saturday': '00011010',
+                 'sunday': '00010110',
+                 'weekend_day_night': '00100010',
+                 'holiday': 'Ngày lễ',
+                 'holiday_day_night': 'Ngày lễ - Ban đêm',
+                 'compensatory_normal': 'Bù ngày lễ vào ngày thường',
+                 'compensatory_night': 'Bù ngày lễ vào ban đêm',
+                 'unknown': 'Không thể xác định'}
+
 
 class OTRegistration(models.Model):
     _name = 'ot.registration'
+    _rec_name = 'employee_id'
     _description = 'OT Registration'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
+    def update_state(self, state):
+        for record in self:
+            record.state = state
+        for record in self.env['ot.registration.line'].sudo().search([('ot_registration_id', '=', self.id)]):
+            record.state = state
+
+    def action_submit(self):
+        self.update_state('to_approve')
 
     @api.depends('ot_registration_lines')
     def _compute_total_ot(self):
@@ -34,16 +61,20 @@ class OTRegistration(models.Model):
                 if item.additional_hours:
                     record.additional_hours += item.additional_hours
 
-    project_id = fields.Many2one('project.project',string='Project')
+    def get_user(self):
+        return self.env['hr.employee'].sudo().search([('user_id', '=', self._uid)], limit=1)
+
+    project_id = fields.Many2one('project.project', string='Project')
     manager_id = fields.Many2one('hr.employee', string='Approver')
     ot_month = fields.Date(string='OT Month', readonly=True)
-    employee_id = fields.Many2one('hr.employee', string='Employee', readonly=True)
+    employee_id = fields.Many2one('hr.employee', string='Employee', readonly=True, default=lambda self: self.get_user())
     company_id = fields.Many2one('res.company', string='Company')
-    dl_id = fields.Many2one('hr.employee', string='Department lead', readonly=True)
+    dl_id = fields.Many2one('hr.employee', string='Department lead', readonly=True, default=2)
     additional_hours = fields.Float(string='Total OT', readonly=True, compute='_compute_total_ot', store=True)
     state = fields.Selection([(key, value) for key, value in STATES_DICT.items()],
                              string='State', default='draft', readonly=True)
-    ot_registration_lines = fields.One2many('ot.registration.line', 'ot_registration_id', string='OT Registration Lines')
+    ot_registration_lines = fields.One2many('ot.registration.line', 'ot_registration_id',
+                                            string='OT Registration Lines')
 
 
 class OTRegistrationLine(models.Model):
@@ -53,34 +84,44 @@ class OTRegistrationLine(models.Model):
     @api.depends('date_from', 'date_to')
     def _compute_category(self):
         vi_holidays = holidays.VN()
+        category_res = ''
         for record in self:
             if record.date_from in vi_holidays or record.date_to in vi_holidays:
-                record.category = 'holiday'
+                category_res = 'holiday'
+            elif record.date_from.weekday() == 5:
+                category_res = 'saturday'
+            else:
+                category_res = 'unknown'
+            record.category = category_res
 
     @api.depends('date_from', 'date_to')
     def _compute_ot_hours(self):
         for record in self:
             ot_seconds = (record.date_to - record.date_from).total_seconds()
-            record.additional_hours = ot_seconds/SECONDS_PER_HOUR if ot_seconds > 0 else 0
+            record.additional_hours = ot_seconds / SECONDS_PER_HOUR if ot_seconds > 0 else 0
 
-    ot_registration_id = fields.Many2one('ot.registration', string='OT Registration ID')
-    employee_id = fields.Many2one('hr.employee', related='ot_registration_id.employee_id')
-    project_id = fields.Many2one('project.project', related='ot_registration_id.project_id')
+    def tz_utc_to_local(self, utc_time):
+        return utc_time + self.utc_offset()
+
+    def utc_offset(self):
+        user_timezone = self.env.user.tz or 'GMT'
+        hours = int(datetime.now(pytz.timezone(user_timezone)).strftime('%z')[:3])
+        return datetime.timedelta(hours=hours)
+
+    ot_registration_id = fields.Many2one('ot.registration', string='OT Registration ID', ondelete='cascade')
+    employee_id = fields.Many2one('hr.employee', related='ot_registration_id.employee_id', store=True)
+    project_id = fields.Many2one('project.project', related='ot_registration_id.project_id', store=True)
     date_from = fields.Datetime(string='From', default=fields.datetime.now(), required=True)
     date_to = fields.Datetime(string='To', default=fields.datetime.now(), required=True)
     category = fields.Selection([(key, value) for key, value in CATEGORY_DICT.items()], string='OT Category',
-                                compute='_compute_category')
+                                compute='_compute_category', store=True)
     is_wfh = fields.Boolean(string='WFH')
     is_intern = fields.Boolean(string='Is intern', default=False)
-    additional_hours = fields.Float(string='Total OT', readonly=True, store=True, compute='_compute_ot_hours')
+    additional_hours = fields.Float(string='Total OT', readonly=True, compute='_compute_ot_hours', store=True)
     job_taken = fields.Char(string='Job Taken')
     is_late_approved = fields.Boolean(string='Late Approved', readonly=True)
     hr_notes = fields.Text(string='HR Notes', readonly=True)
-    attendance_notes = fields.Text(string='Attendance Notes')
-    notes = fields.Char(string='Warning')
-    state = fields.Selection([('draft', 'Draft'),
-                              ('to_approve', 'To Approve'),
-                              ('approved', 'PM Approved'),
-                              ('done', 'DL Approved'),
-                              ('refused', 'Refused')],
-                             string='State', default='draft', readonly=True)
+    attendance_notes = fields.Text(string='Attendance Notes', readonly=True)
+    notes = fields.Char(string='Warning', default='Exceed OT plan', readonly=True)
+    state = fields.Selection([(key, value) for key, value in STATES_DICT.items()],
+                             string='State', default='draft', readonly=True, store=True)
