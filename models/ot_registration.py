25fe69e84635fb1d253d6de54e5a1301d5f2bd5e
DAC Tran Ngoc (VTI.VMS) <dac.tranngoc@vti.vn>
2023-01-09 11:44:46 +0700

    category - hours

--- models/ot_registration.py
+++ models/ot_registration.py
@@ -2,7 +2,8 @@ import datetime
 import holidays
 
 import pytz
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 SECONDS_PER_HOUR = 3600
 
@@ -23,22 +24,6 @@ CATEGORY_DICT = {'normal_day': 'Ngày bình thường',
                  'compensatory_normal': 'Bù ngày lễ vào ngày thường',
                  'compensatory_night': 'Bù ngày lễ vào ban đêm',
                  'unknown': 'Không thể xác định'}
-
-# 6h-8h, 18h30-22h, 22h-6h, 6h-22h
-# morning, normal, night, day, saturday, sunday, weekend, holiday
-CATEGORY_CODE = {'normal_day': '01010000',
-                 'normal_day_morning': '01010000',
-                 'normal_day_night': '00100000',
-                 'saturday': '00011010',
-                 'sunday': '00010110',
-                 'weekend_day_night': '00100010',
-                 'holiday': 'Ngày lễ',
-                 'holiday_day_night': 'Ngày lễ - Ban đêm',
-                 'compensatory_normal': 'Bù ngày lễ vào ngày thường',
-                 'compensatory_night': 'Bù ngày lễ vào ban đêm',
-                 'unknown': 'Không thể xác định'}
-
-
 class OTRegistration(models.Model):
     _name = 'ot.registration'
     _rec_name = 'employee_id'
@@ -81,31 +66,92 @@ class OTRegistrationLine(models.Model):
     _name = 'ot.registration.line'
     _description = 'OT Registration Line'
 
-    @api.depends('date_from', 'date_to')
-    def _compute_category(self):
-        vi_holidays = holidays.VN()
-        category_res = ''
+    @api.constrains('category')
+    def _check_category(self):
         for record in self:
-            if record.date_from in vi_holidays or record.date_to in vi_holidays:
-                category_res = 'holiday'
-            elif record.date_from.weekday() == 5:
-                category_res = 'saturday'
-            else:
-                category_res = 'unknown'
-            record.category = category_res
+            if record.category == 'unknown':
+                raise ValidationError(_('Category khong hop le'))
+    def compute_is_late_approved(self):
+        pass
+    def time_type(self, date_from, date_to):
+        if datetime.datetime(date_from.year, date_from.month, date_from.day, 6, 0, 0, 0) \
+                <= date_from \
+                < date_to \
+                <= datetime.datetime(date_from.year, date_from.month, date_from.day, 8, 30, 0, 0):
+            return 'morning'
+        if datetime.datetime(date_from.year, date_from.month, date_from.day, 18, 30, 0, 0) \
+                <= date_from \
+                < date_to \
+                <= datetime.datetime(date_from.year, date_from.month, date_from.day, 22, 00, 0, 0):
+            return 'normal'
+        if datetime.datetime(date_from.year, date_from.month, date_from.day, 6, 0, 0, 0) \
+                <= date_from \
+                < date_to \
+                <= datetime.datetime(date_from.year, date_from.month, date_from.day, 22, 00, 0, 0):
+            return 'day'
+        if datetime.datetime(date_from.year, date_from.month, date_from.day, 22, 0, 0, 0) \
+                <= date_from \
+                < date_to \
+                <= datetime.datetime(date_from.year, date_from.month, date_from.day + 1, 6, 00, 0, 0):
+            return 'night'
+        return 'unknown'
+    def date_type(self, date_from, date_to):
+        vi_holidays = holidays.VN()
+        if date_from in vi_holidays or date_to in vi_holidays:
+            return 'holiday'
+        if date_from.weekday() == 5:
+            return 'saturday'
+        if date_from.weekday() == 6:
+            return 'sunday'
+        return 'unknown'
 
     @api.depends('date_from', 'date_to')
+    def _compute_category(self):
+        for record in self:
+            date_type = self.date_type(self.tz_utc_to_local(record.date_from), self.tz_utc_to_local(record.date_to))
+            time_type = self.time_type(self.tz_utc_to_local(record.date_from), self.tz_utc_to_local(record.date_to))
+            if date_type == 'holiday':
+                if time_type == 'morning' or 'normal' or 'day':
+                    record.category = 'holiday'
+                elif time_type == 'night':
+                    record.category = 'holiday_day_night'
+                else:
+                    record.category = 'unknown'
+            elif date_type == 'saturday':
+                if time_type == 'morning' or 'normal' 'day':
+                    record.category = 'saturday'
+                elif time_type == 'night':
+                    record.category = 'weekend_day_night'
+                else:
+                    record.category = 'unknown'
+            elif date_type == 'sunday':
+                if time_type == 'morning' or 'normal' or 'day':
+                    record.category = 'sunday'
+                elif time_type == 'night':
+                    record.category = 'weekend_day_night'
+                else:
+                    record.category = 'unknown'
+            else:
+                if time_type == 'morning':
+                    record.category = 'normal_day_morning'
+                elif time_type == 'normal':
+                    record.category = 'normal'
+                elif time_type == 'night':
+                    record.category = 'normal_day_night'
+                else:
+                    record.category = 'unknown'
+    @api.depends('date_from', 'date_to', 'category')
     def _compute_ot_hours(self):
         for record in self:
             ot_seconds = (record.date_to - record.date_from).total_seconds()
-            record.additional_hours = ot_seconds / SECONDS_PER_HOUR if ot_seconds > 0 else 0
+            record.additional_hours = ot_seconds / SECONDS_PER_HOUR if record.category != 'unknown' else 0
 
     def tz_utc_to_local(self, utc_time):
         return utc_time + self.utc_offset()
 
     def utc_offset(self):
         user_timezone = self.env.user.tz or 'GMT'
-        hours = int(datetime.now(pytz.timezone(user_timezone)).strftime('%z')[:3])
+        hours = int(datetime.datetime.now(pytz.timezone(user_timezone)).strftime('%z')[:3])
         return datetime.timedelta(hours=hours)
 
     ot_registration_id = fields.Many2one('ot.registration', string='OT Registration ID', ondelete='cascade')
@@ -118,8 +164,8 @@ class OTRegistrationLine(models.Model):
     is_wfh = fields.Boolean(string='WFH')
     is_intern = fields.Boolean(string='Is intern', default=False)
     additional_hours = fields.Float(string='Total OT', readonly=True, compute='_compute_ot_hours', store=True)
-    job_taken = fields.Char(string='Job Taken')
-    is_late_approved = fields.Boolean(string='Late Approved', readonly=True)
+    job_taken = fields.Char(string='Job Taken', default='N/A')
+    is_late_approved = fields.Boolean(string='Late Approved', readonly=True, compute='compute_is_late_approved', store=True)
     hr_notes = fields.Text(string='HR Notes', readonly=True)
     attendance_notes = fields.Text(string='Attendance Notes', readonly=True)
     notes = fields.Char(string='Warning', default='Exceed OT plan', readonly=True)
